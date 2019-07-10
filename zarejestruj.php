8b96576b535b03d64fb3af96d7b5eb87dd27a555
ProgramistaZpolski <ProgramistaZpolski@users.noreply.github.com>
2019-07-10 14:58:19 +0200

    zmiana na post

--- zarejestruj.php
+++ zarejestruj.php
@@ -1,14 +1,12 @@
 <?php
-echo($_GET["usr"]);
-echo($_GET["usrold"]);
+echo($_POST["usr"]);
+echo($_POST["usrold"]);
 echo readfile("potwier.html");
-mkdir("apps/".$_GET["usr"]);
-echo($_GET["usr"]);
-echo($_GET["usrold"]);
+mkdir("apps/".$_POST["usr"]);
 echo("<h1>20%</h1>");
-copy("whattoinsert.html", "apps/".$_GET["usr"]."/index.html");
+copy("whattoinsert.html", "apps/".$_POST["usr"]."/index.html");
 echo("<h1>60%</h1>");
-$file_handle = fopen("apps/".$_GET["usr"]."/index.html", 'w');
-fwrite($file_handle, '<meta http-equiv="refresh" content="0; url='.$_GET["usrold"].'"/>');
+$file_handle = fopen("apps/".$_POST["usr"]."/index.html", 'w');
+fwrite($file_handle, '<meta http-equiv="refresh" content="0; url='.$_POST["usrold"].'"/>');
 echo("<h1>gotowe!</h1>");
 ?>
